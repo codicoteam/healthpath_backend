573caaa05c80522b4bbe4f61f0542b5c7fe3e3b6
Tapiwa Maunganidze <165782570+codicoteam@users.noreply.github.com>
2025-01-20 04:05:38 +0200

    Update message_models.js

--- models/chat_models/message_models.js
+++ models/chat_models/message_models.js
@@ -4,6 +4,7 @@ const messageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   groupId: { type: String, required: true },
   senderId: { type: String, required: true },
+  first_name: { type: String, required: true },
   message: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
   images: { type: [String], default: [] } // List of image locations (optional field)
