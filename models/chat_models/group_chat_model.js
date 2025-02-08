1b90a3b12d7c42e153de1be19d3d206d951e14f2
Tapiwa Maunganidze <165782570+codicoteam@users.noreply.github.com>
2025-02-08 04:41:39 +0200

    Update group_chat_model.js

--- models/chat_models/group_chat_model.js
+++ models/chat_models/group_chat_model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
   groupId: { type: String, required: true, unique: true },
-  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Array of user IDs
+  users: [{ type: String, ref: 'User' }], // Store userId instead of ObjectId
 });
 
 module.exports = mongoose.model('Group', groupSchema);
